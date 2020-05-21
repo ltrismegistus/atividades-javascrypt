851d3a0c1cbb781bf5aeb76cd577169e223513f3
trismegisto <trismegistolucas1@gmail.com>
2020-05-21 08:38:26 -0300

    respondida a atividade 01

---
+++ cod3r/01.js
@@ -0,0 +1,12 @@
+/* 01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a 
+soma, subtração, multiplicação e divisão desses valores. */
+
+let aritimetica = function(valor1,valor2) {
+    console.log(valor1 + valor2)
+    console.log(valor1 - valor2)
+    console.log(valor1 * valor2)
+    console.log(valor1 / valor2)
+    return
+}
+
+aritimetica(1,0)
